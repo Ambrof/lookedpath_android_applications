c5780c059b00bf343b16bf67f897f9fd093cc296
Simone de Blasiis <simone.deblasiis@gmail.com>
2012-10-04 20:15:15 +0200

    Ultimate Fix

---
+++ PrimaLezione/src/com/lookedpath/firstlesson/About.java
@@ -0,0 +1,20 @@
+package com.lookedpath.firstlesson;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class About extends Activity { 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+    
+    public void goToSite (View view) {
+    	Intent intent = new Intent(Intent.ACTION_VIEW);
+    	intent.setData(Uri.parse("http://www.lookedpath.tk"));
+    	startActivity(intent);
+    }
+}
