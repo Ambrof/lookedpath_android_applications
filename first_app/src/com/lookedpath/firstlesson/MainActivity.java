088b30a0bfaf7ff2c3f35cfa3f3e7a654ba0669f
Simone de Blasiis <simone.deblasiis@gmail.com>
2012-10-04 19:24:11 +0200

    added info menu

--- first_app/src/com/lookedpath/firstlesson/MainActivity.java
+++ first_app/src/com/lookedpath/firstlesson/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lookedpath.firstlesson;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -24,4 +27,16 @@ public class MainActivity extends Activity {
     public void goToMain (View view) {
     	setContentView(R.layout.activity_main);
     }
+    public void goToInfo (View view) {
+    	setContentView(R.layout.info);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings: 
+            startActivity(new Intent(this, About.class));
+        }
+        return true;
+    }
+
 }
