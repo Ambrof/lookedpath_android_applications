753071e1644e278f3231d171f7ab6ed2710c87c2
Simone de Blasiis <simone.deblasiis@gmail.com>
2012-11-03 16:33:15 +0100

    Removal of layout replaced with the settext method
    Should decrease application dimension and should improve prestations

--- PrimaLezione/gen/com/lookedpath/firstlesson/R.java
+++ PrimaLezione/gen/com/lookedpath/firstlesson/R.java
@@ -29,7 +29,6 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int info=0x7f030001;
-        public static final int next=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -37,7 +36,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int button=0x7f050004;
-        public static final int hello_world=0x7f050001;
+        public static final int first=0x7f050001;
         public static final int info=0x7f050006;
         public static final int info2=0x7f050007;
         public static final int menu_settings=0x7f050002;
--- PrimaLezione/src/com/lookedpath/firstlesson/MainActivity.java
+++ PrimaLezione/src/com/lookedpath/firstlesson/MainActivity.java
@@ -2,18 +2,21 @@ package com.lookedpath.firstlesson;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
-
+	private TextView testo;
+	private boolean  next=false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testo= (TextView) findViewById(R.id.textView1);
     }
 
     @Override
@@ -22,20 +25,21 @@ public class MainActivity extends Activity {
         return true;
     }
     public void goToNext (View view) {
-    	setContentView(R.layout.next);
-    }
-    public void goToMain (View view) {
-    	setContentView(R.layout.activity_main);
+    	if(next==false) {
+    		next=true;
+    		testo.setText(R.string.next);
+    	} else {
+    		next=false;
+    		testo.setText(R.string.first);
+    	}
+
     }
     public void goToInfo (View view) {
     	setContentView(R.layout.info);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_settings: 
-            startActivity(new Intent(this, About.class));
-        }
+    	if  (item.getItemId()==R.id.menu_settings) startActivity(new Intent(this, About.class));
         return true;
     }
 
